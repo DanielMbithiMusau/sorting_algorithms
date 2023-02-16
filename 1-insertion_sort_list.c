40e9db17fbbbbae143a5926189c4b9c7bb363882
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-02-16 17:04:20 +0300

    Insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * isertion_sort_list - Function that sorts a doubly linked list
+ * insertion_sort_list - Function that sorts a doubly linked list
  * of integers in ascending order using insertion sort alg.
  * @list: list to be sorted.
  *
@@ -10,38 +10,37 @@
 void insertion_sort_list(listint_t **list)
 {
 	listint_t *current, *temp;
-	
+
 	if (list == NULL || *list == NULL)
 		return;
-	
+
 	current = (*list)->next;
-	
+
 	while (current != NULL)
 	{
 		temp = current->prev;
-		
+
 		while (temp != NULL && temp->n > current->n)
 		{
 			temp->next = current->next;
-			
+
 			if (current->next != NULL)
 				current->next->prev = temp;
-			
+
 			current->next = temp;
 			current->prev = temp->prev;
-			
+
 			if (temp->prev != NULL)
 				temp->prev->next = current;
 			temp->prev = current;
-			
+
 			if (current->prev == NULL)
 				*list = current;
 			/* print list after swapping */
 			print_list(*list);
-			
+
 			temp = current->prev;
 		}
-		
 		current = current->next;
 	}
 }
